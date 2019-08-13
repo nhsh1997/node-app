a2ff6084dd3c2d13e7cf63d2bb10260b41f66903
Hoang Nguyen <nhsh1997@gmail.com>
2019-08-13 14:40:08 +0700

    update test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var request = require('supertest');
-var app = require('../index.js');describe('GET /', function() {
+var app = require('../app.js');describe('GET /', function() {
   it('respond with hello world', function(done) { //navigate to root and check the the response is "hello world"
     request(app).get('/').expect('hello world', done);
   });
