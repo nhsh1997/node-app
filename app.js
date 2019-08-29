f8194e9011c5a39e66ec6b32e7f3d0ea1f485d28
Hoang Nguyen <nhsh1997@gmail.com>
2019-08-29 15:38:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var express = require('express');
 
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-  console.log('hello');
-res.send('hello world');
+  res.send('hi world');
 });
 
 module.exports = app;
